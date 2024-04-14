b8508d545b2df88fbd97d5afc75c75759c9fb695
Jules <jules.delobelle@free.fr>
2024-04-14 16:26:38 +0200

    First push

---
+++ src/main/java/fr/akaazee/factionpicks/FactionPicks.java
@@ -0,0 +1,17 @@
+package fr.akaazee.factionpicks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FactionPicks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("test");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
